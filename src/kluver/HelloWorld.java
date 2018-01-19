6a6f79cf3460432988617c79eead70b889e7bccd
dkluver <dkluver@macalester.edu>
2018-01-19 14:35:47 -0600

    Updated test file

--- src/kluver/HelloWorld.java
+++ src/kluver/HelloWorld.java
@@ -6,6 +6,6 @@ package kluver;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.print("Hello Kluver's Students! What up?");
     }
 }
