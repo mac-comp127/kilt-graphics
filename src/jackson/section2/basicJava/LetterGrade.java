59f817fbff22bbd969abb844c566d2cdcbc56ab8
bretjackson <bjackson@macalester.edu>
2018-01-24 10:18:51 -0600

    Updated lettergrade for sec 2

--- src/jackson/section2/basicJava/LetterGrade.java
+++ src/jackson/section2/basicJava/LetterGrade.java
@@ -8,17 +8,31 @@ import java.util.Scanner;
  */
 public class LetterGrade {
 
+    public static final int NUM_GRADES = 5;
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
+        /*
         System.out.print("Enter a grade: ");
         double grade1 = scan.nextDouble();
         System.out.print("\nEnter a grade: ");
         double grade2 = scan.nextDouble();
         System.out.print("\nEnter a grade: ");
         double grade3 = scan.nextDouble();
+        */
 
-        double avg = (grade1+grade2+grade3)/3;
+        //Accumulator var
+        double total = 0;
+
+
+        for(int i = 0; i < NUM_GRADES; i++){
+            System.out.print("\nEnter a grade: ");
+            total += scan.nextDouble();
+        }
+
+
+        double avg = (total)/NUM_GRADES;
         System.out.println("Your average was: " + avg );
         if (avg >=90.0){
             System.out.println("You got an A!");
