e9274eebf22c962797a4b9d15a2bd02a8e8d2535
dkluver <dkluver@macalester.edu>
2018-01-26 12:48:36 -0600

    Added code from in-class

--- src/kluver/examples/ScratchCode.java
+++ src/kluver/examples/ScratchCode.java
@@ -4,19 +4,8 @@ public class ScratchCode {
     private static final int TOMATO = 3;
 
     public static void main(String[] args) {
-        if (tacoNumber(2) < 4) {
-            System.out.println("too few tacos!");
-        } else if (tacoNumber(2) == 6) {
-            System.out.println("6 tacos");
-        } else if (tacoNumber(2) <= 5) {
-            System.out.println("5 tacos");
-        } else {
-
+        for (double d = 0.0; d != 1; d+=0.1) {
+            System.out.println(d);
         }
-    }
-
-    public static int tacoNumber(int lettuce) {
-        System.out.println("!");
-        return lettuce * TOMATO;
     }
 }
---
+++ src/kluver/examples/SumRange.java
@@ -0,0 +1,31 @@
+package kluver.examples;
+
+import java.util.Scanner;
+
+/**
+ * An example showing how different types of loops can be used to look for user
+ * input, and perform summations.
+ */
+public class SumRange {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the lower bound");
+        System.out.print(">> ");
+        int lower = input.nextInt();
+
+        int upper;
+        do {
+            System.out.println("Input the upper bound, (>="+lower+")");
+            System.out.print(">> ");
+            upper = input.nextInt();
+        } while (upper < lower);
+
+        int sum =0 ;
+        for(int i = lower; i<= upper; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("The sum from "+lower+" to "+upper+" is "+sum);
+    }
+}
