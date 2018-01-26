41bb27b25308cbe0807ad15b5748a26097c9b3f3
bretjackson <bjackson@macalester.edu>
2018-01-26 07:59:46 -0600

    Added loop examples for sec1/2

---
+++ src/jackson/section2/loopExamples/WhileLoopExample.java
@@ -0,0 +1,28 @@
+package jackson.section2.loopExamples;
+
+import java.util.Scanner;
+
+/**
+ * Example of a repeat until sentinel loop
+ * Created by bjackson on 9/9/2016.
+ */
+public class WhileLoopExample {
+
+    public static final String SENTINEL = "STOP";
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int sum = 0;
+
+        String input = scan.nextLine();
+        // Note the use of .equals() rather than ==. This is because we are comparing strings which are a class.
+        while(!input.equals(SENTINEL)){
+            sum += input.length();
+            input = scan.nextLine();
+        }
+        System.out.println("You typed "+sum+" letters");
+
+
+    }
+}
