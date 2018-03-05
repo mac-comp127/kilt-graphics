69c3b7e0e8ee03b2f4d337839187880c5ee9d74c
bretjackson <bjackson@macalester.edu>
2018-03-05 10:19:56 -0600

    Added sorting example to sec 2

---
+++ src/jackson/section2/sortingExamples/Media.java
@@ -0,0 +1,71 @@
+package jackson.section2.sortingExamples;
+
+/**
+ * A media object in an iTunes library that may be a
+ * podcast, song, video, etc.
+ * 
+ * @author shilad
+ */
+public class Media implements Comparable<Media> {
+	
+	private String filePath;
+	private String name;
+	private int count;
+	
+	public Media(String filePath, String name, int count) {
+		this.filePath = filePath;
+		this.name = name;
+		this.count = count;
+	}
+
+	public Media(String filePath, String name) {
+        this(filePath, name, 0);
+	}
+
+	@Override
+	public String toString() {
+		return "media: " + name + " at " + filePath + " with count " + count;
+	}
+
+	public boolean matchesQuery(String query) {
+		return getName().toLowerCase().contains(query.toLowerCase());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	@Override
+    public boolean equals(Object other) {
+		if (other == null || !(other instanceof Media)){
+            return false;
+        }
+
+        Media media = (Media) other;
+        return (filePath.equals(media.filePath) && name.equals(media.name));
+    }
+
+    public int compareTo(Media other){
+		/*
+		if (this.count < other.count){
+			return -1;
+		}
+		else if (this.count > other.count){
+			return 1;
+		}
+		else {
+			return 0;
+		}
+		*/
+
+		return this.count - other.count;
+	}
+}
