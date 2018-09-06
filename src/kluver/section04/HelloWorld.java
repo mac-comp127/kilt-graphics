e42baad499505f246974b2a9505710b85b00f4dc
dkluver <dkluver@macalester.edu>
2018-09-06 13:44:19 -0500

    removed some old crap from last semester, also added example.

---
+++ src/kluver/section04/HelloWorld.java
@@ -0,0 +1,8 @@
+package kluver.section04;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("goodbye world, for I see the hulk, and the end must be near.");
+    }
+}
