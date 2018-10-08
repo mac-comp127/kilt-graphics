957ed9b4a01a6a9b805a6a1a92966a518b9d1da1
dkluver <dkluver@macalester.edu>
2018-10-08 14:21:32 -0500

    In-class examples (changes from a few days)

--- src/kluver/section05/graphicsGroupExample/Animation.java
+++ src/kluver/section05/graphicsGroupExample/Animation.java
@@ -1,6 +1,7 @@
 package kluver.section05.graphicsGroupExample;
 
 import comp124graphics.CanvasWindow;
+import comp124graphics.GraphicsGroup;
 import comp124graphics.Triangle;
 
 import java.awt.*;
@@ -8,17 +9,18 @@ import java.awt.*;
 public class Animation {
     public static void main(String[] args) {
         CanvasWindow canvas = new CanvasWindow("Animation", 800, 600);
-        Triangle thingToGlide = new Triangle(50,100, 0,150,100,200);
-        thingToGlide.setFillColor(Color.RED);
-        thingToGlide.setFilled(true);
 
-        canvas.add(thingToGlide);
+        Flower flower = new Flower(0, 100, 400);
 
-        while(thingToGlide.getX()<=canvas.getWidth()) {
-            thingToGlide.move(1,0);
-            canvas.pause(1);
+        canvas.add(flower);
+
+        while(true) {
+            flower.move(1,0); // change what we show
+            canvas.pause(10);
+            if(flower.getX()>canvas.getWidth()) {
+                flower.setPosition(-flower.getWidth(), flower.getY());
+            }
         }
-        canvas.closeWindow();
 
     }
 
