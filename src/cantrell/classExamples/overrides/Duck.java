141fa8c9071883b618c67722563d6993a620c855
Paul Cantrell <cantrell@pobox.com>
2018-10-09 13:33:05 -0500

    Duck example

--- src/cantrell/classExamples/overrides/Duck.java
+++ src/cantrell/classExamples/overrides/Duck.java
@@ -2,6 +2,10 @@ package cantrell.classExamples.overrides;
 
 public class Duck {
 
+    public static Duck giveMeADuck() {
+        return new Duck();
+    }
+
     public void quack() {
         System.out.println("Quack.");
     }
