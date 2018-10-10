20c9467f5fea26d7b6bb63722aa9a0afbe5239dc
lduan-mac <lduan@macalester.edu>
2018-10-10 08:49:36 -0500

    Finished HW 2 (most), in class activities

--- test/cantrell/classExamples/CollatzIteratorTest.java
+++ test/cantrell/classExamples/CollatzIteratorTest.java
@@ -1,6 +1,8 @@
 package cantrell.classExamples;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollatzIteratorTest {
