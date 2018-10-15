50f9d7355b7f0c2b43f396a55c751a4890e97596
dkluver <dkluver@macalester.edu>
2018-10-15 14:55:35 -0500

    Dice Tester complete section 4

--- src/kluver/section04/DiceTester.java
+++ src/kluver/section04/DiceTester.java
@@ -1,12 +1,54 @@
 package kluver.section04;
 
+import comp124graphics.CanvasWindow;
+import comp124graphics.GraphicsText;
+import comp124graphics.Rectangle;
+
+import java.awt.*;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class DiceTester {
     private String fileName;
+    private int[] counts;
 
     public DiceTester(String fileName) {
         this.fileName = fileName;
+        this.counts = count(fileName);
+    }
+
+    private int[] count(String fileName) {
+        InputStream is = openResource(fileName);
+        Scanner scan = new Scanner(is);
+
+        int[] retVal = new int[6];
+        // assume retVal is a bunch of zeros.
+
+        while(scan.hasNextInt()) {
+            int next = scan.nextInt();
+            retVal[next-1] = retVal[next-1]+1;
+        }
+        return retVal;
+    }
+
+    private CanvasWindow plot() {
+        CanvasWindow cw = new CanvasWindow("plot", 600, 600);
+        for(int i = 0; i<counts.length; i++) {
+            int width=100;
+            int height = counts[i]/2;
+            int x = i*100;
+            int y = cw.getHeight()-height;
+            Rectangle rect = new Rectangle(x,y,width,height);
+            rect.setFillColor(Color.lightGray);
+            rect.setFilled(true);
+            cw.add(rect);
+        }
+
+        GraphicsText gt = new GraphicsText(this.fileName, 0, 10);
+        cw.add(gt);
+
+        return cw;
     }
 
     /**
@@ -18,7 +60,7 @@ public class DiceTester {
      * read the file from a marked reasource folder. This let's intellj deal with
      * issues between your sytem and mine.
      *
-     * @param fileName - the name of the file <b> in the res folder</b> that we
+     * @param fileName  the name of the file <b> in the res folder</b> that we
      *                 want to read.
      *
      * @return an InputStream - a low level input/output (IO) class that allows
@@ -30,6 +72,11 @@ public class DiceTester {
         return DiceTester.class.getResourceAsStream(fileName);
     }
 
+    @Override
+    public String toString() {
+        return this.fileName+": "+ Arrays.toString(this.counts);
+    }
+
     public static void main(String[] args) {
         DiceTester fair = new DiceTester("/knownFairDice.txt");
         DiceTester unknown = new DiceTester("/unknownDice.txt");
@@ -39,5 +86,7 @@ public class DiceTester {
         System.out.println(unknown);
 
         // plot the counts
+        fair.plot();
+        unknown.plot();
     }
 }
