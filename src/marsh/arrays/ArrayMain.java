3991bdcb051501882f39be52ce4c9b3791e23d24
Abigail Marsh <amarsh1@macalester.edu>
2019-03-01 14:19:47 -0600

    Updates from Sec 02

--- src/marsh/arrays/ArrayMain.java
+++ src/marsh/arrays/ArrayMain.java
@@ -3,19 +3,22 @@ package marsh.arrays;
 public class ArrayMain {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        int numbers[];
+
+        numbers = new int[5];
 
         numbers[0] = 1;
-        numbers[1] = 2;
+        numbers[1] = 3;
         numbers[2] = 3;
-        numbers[3] = 4;
+        numbers[3] = 3;
         numbers[4] = 5;
+
 
         for(int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
 
-        Animal[] livestock = new Animal[2];
+        Animal[] livestock = new Animal[3];
 
         Horse harry = new Horse("Harry");
         Cow daisy = new Cow("Daisy");
@@ -24,8 +27,15 @@ public class ArrayMain {
         livestock[1] = daisy;
 
         for(Animal a : livestock) {
-            System.out.println("Animal " + a.getName() + " says " + a.vocalize());
+            //System.out.println("Animal " + a.getName() + " says " + a.vocalize());
+            System.out.println("In the livestock array");
         }
+
+        Animal[] livestock2 = new Animal[3];
+
+        livestock2[1] = daisy;
+
+        System.out.println(livestock2[1].getName());
 
 
 
