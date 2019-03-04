2d0196ced9b9e9693dd42f071e2ac7256a4682c3
Shilad Sen <Shilad.Sen@target.com>
2019-03-04 07:58:32 -0600

    Using a for-each loop.

--- src/sen/animals2/AnimalMain.java
+++ src/sen/animals2/AnimalMain.java
@@ -3,7 +3,8 @@ package sen.animals2;
 public class AnimalMain {
     public static void main(String [] args) {
         Animal [] pets = new Animal[2];
-        pets[0] = new Dog("fido", "brown");
+        Animal fido = new Dog("fido", "brown");
+        pets[0] = fido;
         pets[1] = new Cat("tom", "gray");
         for (Animal p : pets) {
             System.out.println("pet  " + p.getName() + " says " + p.vocalize());
