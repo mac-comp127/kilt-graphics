68b276b08f5c374e4b9bd527046b843370a40936
Abigail Marsh <amarsh1@macalester.edu>
2019-03-11 10:27:50 -0500

    Starter code for lists discussion

---
+++ src/marsh/arraylists/Animal.java
@@ -0,0 +1,8 @@
+package marsh.arraylists;
+
+public interface Animal {
+    public String vocalize();
+    public String getName();
+    public String getSpecies();
+
+}
---
+++ src/marsh/arraylists/ListMain.java
@@ -0,0 +1,24 @@
+package marsh.arraylists;
+
+public class ListMain {
+
+    public static void main(String[] args) {
+        //Create a list of animals
+
+
+        //Add animals to list
+
+
+        //sort the list
+
+
+        //Print the list, iterate through the list
+
+
+        //Remove items by index, object
+
+
+
+
+    }
+}
