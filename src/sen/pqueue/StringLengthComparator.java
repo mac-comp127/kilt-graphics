d5fa90ed1b14324677ffa59bace0f161ed4ee3c8
Shilad Sen <Shilad.Sen@target.com>
2019-04-24 08:59:00 -0500

    Priority Queue methods

---
+++ src/sen/pqueue/StringLengthComparator.java
@@ -0,0 +1,10 @@
+package sen.pqueue;
+
+import java.util.Comparator;
+
+public class StringLengthComparator implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+}
