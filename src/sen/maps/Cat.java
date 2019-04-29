7b75924199f01e69fed9cb6c3ef0ba0d5da338e8
Shilad Sen <Shilad.Sen@target.com>
2019-04-29 08:52:15 -0500

    TreeMap example

---
+++ src/sen/maps/Cat.java
@@ -0,0 +1,38 @@
+package sen.maps;
+
+import java.util.Objects;
+
+public class Cat extends Animal {
+    private String color;
+
+    public Cat(String name, String color) {
+        super(name);
+        this.color = color;
+    }
+
+    public String vocalize() {
+        return "<meow>";
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + getName() + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return color.equals(cat.color)
+                && getName().equals(cat.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
+    }
+}
