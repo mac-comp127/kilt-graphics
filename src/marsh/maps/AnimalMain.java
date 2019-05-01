e9118de8d906b4a35e83f6d740b18b80d61b20b8
Abigail Marsh <amarsh1@macalester.edu>
2019-05-01 11:25:54 -0500

    Addition of HashMaps to earlier Map activity

--- src/marsh/maps/AnimalMain.java
+++ src/marsh/maps/AnimalMain.java
@@ -1,27 +1,48 @@
 package marsh.maps;
 
-import marsh.pqueue.StringLengthComparator;
-
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class AnimalMain {
     public static void main(String [] args) {
 
-        Map<String, Animal> pet = new TreeMap<>(new StringLengthComparator());
+        // TreeMap backing for the pet Map
+        //Map<String, Animal> pet = new TreeMap<>(new StringLengthComparator());
 
-        pet.put("fido", new Dog("fido", "brown"));
-        pet.put("tom", new Cat("tom", "grey"));
-        pet.put("theophilus", new Dog("theophilus", "red"));
-        pet.put("frank", new Cat("frank", "black"));
+//        Map<String, Animal> pet = new HashMap<>();
+//
+//        pet.put("fido", new Dog("fido", "brown"));
+//        pet.put("tom", new Cat("tom", "grey"));
+//        pet.put("theophilus", new Dog("theophilus", "red"));
+//        pet.put("frank", new Cat("frank", "black"));
+//
+//        for(String name: pet.keySet()) {
+//            System.out.println(name + " is a: " + pet.get(name));
+//
+//        }
 
-        for(String name: pet.keySet()) {
-            System.out.println(name + " is a: " + pet.get(name));
 
+//         Experimenting with a Animals as keys
+        Map<Animal, Integer> reversePets = new HashMap<>();
+
+        Animal fido = new Dog("fido", "brown");
+        reversePets.put(new Cat("hank", "grey"), 3);
+        reversePets.put(new Cat("snowball II", "black"), 5);
+        reversePets.put(fido, 15);
+        reversePets.put(new Cat("daisy", "white"), 9);
+        reversePets.put(new Cat("harry", "orange"), 12);
+
+        fido.setName("FIDO!!");
+
+        System.out.println("Iterating through Keys: ");
+        for(Animal a: reversePets.keySet()) {
+            System.out.println(a + "'s age: " + reversePets.get(a));
         }
 
-
-
+        System.out.println("\nIterating through Entries: ");
+        for(Map.Entry<Animal, Integer> e : reversePets.entrySet() ) {
+            System.out.println(e.getKey() + "'s age: " + e.getValue());
+        }
 
     }
 }
