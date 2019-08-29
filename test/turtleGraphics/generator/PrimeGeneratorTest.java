46f952bb4901c61a4e96fea2cf2bf0de8d584717
Paul Cantrell <cantrell@pobox.com>
2019-08-29 18:09:50 -0500

    Use JUnit 5

--- test/turtleGraphics/generator/PrimeGeneratorTest.java
+++ test/turtleGraphics/generator/PrimeGeneratorTest.java
@@ -1,8 +1,8 @@
 package turtleGraphics.generator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PrimeGeneratorTest {
 
