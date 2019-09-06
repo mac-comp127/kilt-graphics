5dbeee5ef303122af66b7963d8b0f564ef9a4ae0
Abigail Marsh <amarsh1@macalester.edu>
2019-09-06 11:31:43 -0500

    Demo from second day of class

--- src/marsh/Expressions.java
+++ src/marsh/Expressions.java
@@ -3,15 +3,15 @@ package marsh;
 public class Expressions {
 
     public static void main (String[] args) {
-        int x;
+        int x; // Declaring a new integer variable, x
 
-        x = 5;
+        x = 5; // assigning a value to x
 
-        System.out.println("x = " + x);
+        System.out.println("x = " + x);  // Print the value of x, concatenating a string and an int
 
-        int y = ++x;
+        int y = ++x; // Try this with x++ instead! What changes?
 
-        System.out.println("x = " + x + " y = " + y);
+        System.out.println("x = " + x + " y = " + y); // print results of expression above
 
 
     }
