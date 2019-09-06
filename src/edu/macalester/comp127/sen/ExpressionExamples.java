ba2ae91ffc20b57fb18ef93f6f2a9beb654a4906
Shilad Sen <shilad@gmail.com>
2019-09-06 14:00:32 -0500

    Added in class examples.

---
+++ src/edu/macalester/comp127/sen/ExpressionExamples.java
@@ -0,0 +1,13 @@
+package edu.macalester.comp127.sen;
+
+public class ExpressionExamples {
+    public static void main(String [] args) {
+        int x = 5;
+        int y = 4;
+        double z = 4.0;
+        System.out.println(x + y);
+        System.out.println(x + z);
+        System.out.println(x / y);
+        System.out.println(x / z);
+    }
+}
