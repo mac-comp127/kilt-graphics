957d96c9f7943c56809bcdfc44b146fc6d830a2c
Abigail Marsh <amarsh1@macalester.edu>
2019-09-06 11:27:13 -0500

    Demo from second day of class

---
+++ src/marsh/Expressions.java
@@ -0,0 +1,19 @@
+package marsh;
+
+public class Expressions {
+
+    public static void main (String[] args) {
+        int x;
+
+        x = 5;
+
+        System.out.println("x = " + x);
+
+        int y = ++x;
+
+        System.out.println("x = " + x + " y = " + y);
+
+
+    }
+
+}
