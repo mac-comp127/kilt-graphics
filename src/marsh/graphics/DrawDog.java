ba3a8c0e05145bd9a747fdaef715977d1bf29988
Abigail Marsh <amarsh1@macalester.edu>
2019-09-25 10:16:34 -0500

    i'm just very proud of this dog

--- src/marsh/graphics/DrawDog.java
+++ src/marsh/graphics/DrawDog.java
@@ -61,6 +61,9 @@ public class DrawDog {
         bleg2.setStrokeColor(Color.GRAY);
         cw.add(bleg2);
 
+        GraphicsText bark = new GraphicsText("bark", 250, 300);
+        cw.add(bark);
+
         cw.draw();
     }
 }
