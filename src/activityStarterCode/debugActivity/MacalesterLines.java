22a1c7557c3d357aa836845836045d7ee805dd55
Paul Cantrell <cantrell@pobox.com>
2019-11-05 16:43:01 -0600

    Updated debug activity for comp127graphics

---
+++ src/activityStarterCode/debugActivity/MacalesterLines.java
@@ -0,0 +1,57 @@
+package activityStarterCode.debugActivity;
+
+import comp127graphics.*;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dkluver on 11/17/17.
+ */
+public class MacalesterLines extends GraphicsGroup {
+    public void draw() {
+        List<Line> lines = new ArrayList<>();
+        Line line = new Line(0, 0, 0, 0);
+
+        line.setStartPosition(-40, 0);
+        line.setEndPosition(360, 400);
+        line.setStrokeColor(new Color(0xA571C5E8, true));
+        line.setStrokeWidth(60);
+        lines.add(line);
+
+        line.setStartPosition(100, 40);
+        line.setEndPosition(460, 400);
+        line.setStrokeColor(new Color(0xA5002033, true));
+        line.setStrokeWidth(30);
+        lines.add(line);
+
+        line.setStartPosition(100, 430);
+        line.setEndPosition(460, 70);
+        line.setStrokeColor(new Color(0xA5002033, true));
+        line.setStrokeWidth(30);
+        lines.add(line);
+
+        line.setStartPosition(110, 369);
+        line.setEndPosition(470, 9);
+        line.setStrokeColor(new Color(0xA55B6770, true));
+        line.setStrokeWidth(10);
+        lines.add(line);
+
+        line.setStartPosition(0, 430);
+        line.setEndPosition(360, 70);
+        line.setStrokeColor(new Color(0xA5EF4F26, true));
+        line.setStrokeWidth(30);
+        lines.add(line);
+
+        lines.forEach(this::add);
+    }
+
+    public static void main(String[] args) {
+        // should draw a lightning bolt (the flash logo)
+        CanvasWindow window = new CanvasWindow("Macalester lines", 400, 400);
+        MacalesterLines fl = new MacalesterLines();
+        fl.draw();
+        window.add(fl);
+    }
+}
--- src/activityStarterCode/debugActivity/Star.java
+++ src/activityStarterCode/debugActivity/Star.java
@@ -6,6 +6,7 @@ import comp127graphics.Path;
 import comp127graphics.Point;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,45 +17,45 @@ public class Star extends GraphicsGroup {
     public void draw() {
         // get the star shape, centered at (0,0)
         List<Point> starPoints = getStarPoints();
+
+        // make a copy so we can translate the points
+        starPoints = new ArrayList<>(starPoints);
+
         // translate the shape to be correctly centered)
-        translateStarPoints(starPoints, 100, 100);
+        translateStarPoints(starPoints, new Point(100, 100));
 
         Path poly = new Path(starPoints);
         poly.setFillColor(Color.YELLOW);
-        poly.setFilled(true);
         add(poly);
     }
 
-    private void translateStarPoints(List<Point> points, double dx, double dy) {
-        for(int i = 1; i<points.size(); i++) {
+    private void translateStarPoints(List<Point> points, Point delta) {
+        for(int i = 1; i < points.size(); i++) {
             Point p = points.get(i);
-            // Needs rework for new Point class
-//            p.setLocation(p.getX()+dx, p.getY()+dy);
+            points.set(i, p.add(delta));
         }
     }
 
-    // note - these points are all 100% correct. It would be too simple (and
-    // hard to fix) for me to make an error in these points. To be clear, there
-    // is no bug in this method.
+    // Note: these points are all 100% correct.
+    // There is no bug in this method.
     public List<Point> getStarPoints() {
-        List<Point> points = new LinkedList<>();
-        points.add(new Point(58.78,   80.9));
-        points.add(new Point(0.0,     40.0));
-        points.add(new Point(-58.78,  80.9));
-        points.add(new Point(-38.04,  12.36));
-        points.add(new Point(-95.11, -30.9));
-        points.add(new Point(-23.51, -32.36));
-        points.add(new Point(0.0  ,  -100.0));
-        points.add(new Point(23.51,  -32.36));
-        points.add(new Point(95.11,  -30.9));
-        points.add(new Point(38.04,   12.36));
-        return points;
+        return List.of(
+            new Point(58.78,   80.9),
+            new Point(0.0,     40.0),
+            new Point(-58.78,  80.9),
+            new Point(-38.04,  12.36),
+            new Point(-95.11, -30.9),
+            new Point(-23.51, -32.36),
+            new Point(0.0  ,  -100.0),
+            new Point(23.51,  -32.36),
+            new Point(95.11,  -30.9),
+            new Point(38.04,   12.36));
     }
 
     public static void main(String[] args) {
         CanvasWindow window = new CanvasWindow("star", 200, 200);
-        Star group = new Star();
-        group.draw();
-        window.add(group);
+        Star star = new Star();
+        star.draw();
+        window.add(star);
     }
 }
