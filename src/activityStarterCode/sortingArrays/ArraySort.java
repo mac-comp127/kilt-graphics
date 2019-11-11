e5387695f4bce042166dfedd8a282dd759e185b5
Paul Cantrell <cantrell@pobox.com>
2019-11-11 07:41:49 -0600

    Indentation

--- src/activityStarterCode/sortingArrays/ArraySort.java
+++ src/activityStarterCode/sortingArrays/ArraySort.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 public class ArraySort {
 
     private static String[] merge(String[] a, String[] b) {
-            int newLength = a.length + b.length;
+        int newLength = a.length + b.length;
 
-            String[] merged = new String[newLength];
+        String[] merged = new String[newLength];
 
-            for (int i = 0, ai = 0, bi = 0; i < newLength; i++) {
-                if(a[ai].compareTo(b[bi]) < 0) {
-                    merged[i] = a[ai];
-                    ai++;
-                } else {
-                    merged[i] = b[bi];
-                    bi++;
-                }
+        for (int i = 0, ai = 0, bi = 0; i < newLength; i++) {
+            if (a[ai].compareTo(b[bi]) < 0) {
+                merged[i] = a[ai];
+                ai++;
+            } else {
+                merged[i] = b[bi];
+                bi++;
             }
-            return merged;
         }
+        return merged;
+    }
 
     public static String[] sort(String[] a) {
-        if(a.length == 1) {
+        if (a.length == 1) {
             return a;
         }
 
