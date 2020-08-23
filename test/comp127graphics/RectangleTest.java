101ac182218dabf33baa4434bd3863338d8248b9
Paul Cantrell <cantrell@pobox.com>
2020-08-22 21:48:53 -0500

    Revamped Rectangle tests using new modes

--- test/comp127graphics/RectangleTest.java
+++ test/comp127graphics/RectangleTest.java
@@ -1,8 +1,7 @@
 package comp127graphics;
 
+import static comp127graphics.TestRenderingMode.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.awt.Color;
 
 public class RectangleTest implements GraphicsObjectTestSuite {
 
@@ -14,39 +13,28 @@ public class RectangleTest implements GraphicsObjectTestSuite {
     }
     
     @RenderingTest
-    void defaultStyle() {
-        rect = new Rectangle(8, 20, 25, 10);
-    }
-    
-    @RenderingTest
-    // @HitTest // TODO
-    void unfilledCustomStroke() {
-        rect = new Rectangle(new Point(10, 20), new Point(30, 40));
-        assertChangedAtEachStep(
-            () -> rect.setStrokeColor(Color.MAGENTA),
-            () -> rect.setStrokeWidth(12)
-        );
-    }
-    
-    @RenderingTest
-    void filledStroked() {
-        rect = new Rectangle(17.5, 24, 60, 30);
-        assertChangedAtEachStep(
-            () -> rect.setStrokeColor(Color.CYAN),
-            () -> rect.setStrokeWidth(3),
-            () -> rect.setFillColor(Color.BLUE)
-        );
-    }
-    
-    @RenderingTest
-    void filledNoStrokeWithRearrangement() {
+    void regularRect() {
         rect = new Rectangle(17, 24.5, 60.5, 30.1);
         assertChangedAtEachStep(
-            () -> rect.setStroked(false),
-            () -> rect.setFillColor(Color.BLUE),
             () -> rect.setCenter(new Point(45, 60)),
-            () -> rect.setSize(new Point(56.5, 33.3))
+            () -> rect.setSize(new Point(56.5, 33.3)),
+            () -> rect.moveBy(5, 3)
         );
-        assertEquals(new Rectangle(14.75, 44.95, 56.5, 33.3), rect);
+        assertEquals(new Rectangle(19.75, 47.95, 56.5, 33.3), rect);
+    }
+
+    @RenderingTest(modes = { STROKED, FILLED, HIT_TEST })
+    void degenerateRect() {
+        rect = new Rectangle(10, 20, 30, 0);
+    }
+
+    @RenderingTest(modes = { STROKED, FILLED, HIT_TEST })
+    void zeroRect() {
+        rect = new Rectangle(20, 10, 0, 0);
+    }
+
+    @RenderingTest(modes = { STROKED, FILLED, HIT_TEST })
+    void negativeSizeRect() {
+        rect = new Rectangle(80, 70, -60, -50);
     }
 }
